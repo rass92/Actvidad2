abf44f3ff04d08cbd5ca946e68b64d0f0d75af59
andreinacuello <andreinacr48@gmail.com>
2017-03-19 15:16:51 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 #include <math.h>
 #include <string.h>
 #include "./lib/clase_ag.cpp"   //incluyo la libreria que estÃ¡ en carpeta contigua
-#include "./Actvidad2-master/extra_ag2.cpp"  //incluyo libreria
+#include "./lib/extra_ag2.cpp"  //incluyo libreria
 
 #define N_BYTES         400  // longitud mÃ¡xima del string (cromosoma). se utiliza a efectos de reserva de espacio
-#define N_INDIVIDUOS    1 //cantidad de individuos que forman a la poblaciÃ³n
+#define N_INDIVIDUOS    100 //cantidad de individuos que forman a la poblaciÃ³n
 #define N_PASOS        400      //cantidad de pasos maximos que hara el algoritmo
+#define N_MOV        4      //numero de pasos que se pueden realizar: arriba, abajo, izquierda y derecha
 #define N_CASILLAS        22      //longitud de un lado del mapa que es de forma de cuadrada
 
 
@@ -15,7 +16,7 @@ typedef unsigned char uchar;
 typedef unsigned long ulong;
 
 // variables
-int mapa[N_CASILLAS][N_CASILLAS]; //El valor de cada caja
+int map[casillas]; //El valor de cada caja
 
 
 CLASECromosoma a, b, c, d; //varios cromosomas auxiliares
@@ -28,7 +29,8 @@ bool t;
 float evaluacion(uchar *);  //funciÃ³n que recibe el string que forma al cromosoma, lo decodifica, y devuelve su fitness
 void mostrar(uchar *);  //funciÃ³n que recibe al cromosoma y lo muestra en pantalla (visualizaciÃ³n)
 
-int main(){
+int main()
+{
     //apuntador auxiliar a un cromosoma
     CLASECromosoma *apuntador = NULL;
 
@@ -39,16 +41,17 @@ int main(){
 
     //archivo de excel
     archivo = fopen("ejemplo01_evol.xls", "w+");
-
     fprintf(archivo, "Mejor:\tPoblacion:\n"); // \n\r para windows
 
     //Se abre el archivo donde esta el mapa
+    
     mapas = fopen("matriz.prn", "r");
 
 
+    
     t=importTabFile(mapas,mapa,N_CASILLAS);
-
-
+    
+    
     //inicio ciclo de entrenamiento
     do{
         ag.Generacion();    //itero una generaciÃ³n: selecciÃ³n -> cruce -> mutaciÃ³n
@@ -58,21 +61,10 @@ int main(){
         fprintf (archivo, "%.3f\t%.3f\n", apuntador->Fitness(), ag.Fitness());
         //muestro el contenido del cromosoma del mejor
         mostrar(apuntador->Cromosoma);
-    }while(ag.Edad() < 1);    // condicion de parada
+    }while(ag.Edad() < 500);    // condicion de parada
 
     //imprimo la edad de la poblacion: numero de iteraciones totales ejecutadas (generaciones)
-    printf ("\n\tGeneracion: %ld \n", ag.Edad());
-
-
-    for (int i=0; i<N_CASILLAS; i++)
-        {
-
-    for (int j=0; j<N_CASILLAS; j++)
-		{
-            printf("%d ",mapa[i][j]);
-		}
-		printf("\n");
-        }
+    printf ("\n\tGeneracion: %ld", ag.Edad());
     //cierro al archivo correctamente
     fclose(archivo);
     return 0;
@@ -107,12 +99,6 @@ float evaluacion(uchar* cromosoma)
     for (int i=0; i < N_PASOS; i++)
     {
         if(pasosVacios==0){
-            if(i == (N_ALMACEN - 1)){
-                Est_actual = *(cromoRep + i);
-                j = Est_actual%N_ALMACEN;
-                dis_x = x_pos[j] - 0;
-                dis_y = y_pos[j] - 0;
-            }else{
                 pasoActual = *(cromosoma + i); //0,1,2,3
                 pasoSiguente = *(cromosoma + i + 1); //0,1,2,3
 
@@ -132,24 +118,48 @@ float evaluacion(uchar* cromosoma)
                     }
                 }
                 
-                if(k==0){
-                    pos_y = pos_y + 1;
+                if(k==1){
+                    pos_x = pos_x + 1;
                     if(posicion[pos_x][pos_y]==0){
-                        ubi_y = ubi_y + 1; 
+                        ubi_x = ubi_x + 1; 
                     }else if(posicion[pos_x][pos_y]==1){
-                        ubi_y = ubi_y + 1;
+                        ubi_x = ubi_x + 1;
                         repetidos = repetidos + 1;
                     }else{
                          chocados =  chocados + 1;
-                         pos_y = pos_y - 1;
+                         pos_x = pos_x - 1;
                     }
                 }
-
-            }
-            distancia = sqrt((pow(dis_x,2))+(pow(dis_y,2)));
-            dis_total = dis_total + distancia + Penalizacion + distancia_i;
-            Penalizacion = 0;
-            distancia_i = 0;
+            
+                if(k==2){
+                    pos_y = pos_y - 1;
+                    if(posicion[pos_x][pos_y]==0){
+                        ubi_y = ubi_y - 1; 
+                    }else if(posicion[pos_x][pos_y]==1){
+                        ubi_y = ubi_y - 1;
+                        repetidos = repetidos + 1;
+                    }else{
+                         chocados =  chocados + 1;
+                         pos_y = pos_y + 1;
+                    }
+                }
+            
+                if(k==3){
+                    pos_x = pos_x - 1;
+                    if(posicion[pos_x][pos_y]==0){
+                        ubi_x = ubi_x - 1; 
+                    }else if(posicion[pos_x][pos_y]==1){
+                        ubi_x = ubi_x - 1;
+                        repetidos = repetidos + 1;
+                    }else{
+                         chocados =  chocados + 1;
+                         pos_x = pos_x + 1;
+                    }
+                }
+            //distancia = sqrt((pow(dis_x,2))+(pow(dis_y,2)));
+            //dis_total = dis_total + distancia + Penalizacion + distancia_i;
+            //Penalizacion = 0;
+            //distancia_i = 0;
         }else{
             i=N_PASOS;
         }
@@ -158,7 +168,7 @@ float evaluacion(uchar* cromosoma)
     //Para ello chequeo si excedo del mÃ¡ximo permitido. Si estoy dentro del rango, penalizo
 
     //ahora calculo el fitness, o valor neto, en funciÃ³n del ValorTotal, la Penalizacion y el VolumenRest
-    fitness = 1/(1 + dis_total);
+    fitness = 1/(1 + repetidos + chocados);
     return fitness;
 }
 
@@ -174,18 +184,91 @@ float evaluacion(uchar* cromosoma)
 void mostrar(uchar* cromosoma)
 {
     float fitness = 0;      //auxiliar en el cÃ¡lculo de valor o fitness de la soluciÃ³n
-    float Penalizacion = 0; //valor de penalizacion si solucion no es valida
-    int Est_actual;
-    int Est_siguente;
-    int j=0, k=0;
-    int dis_x, dis_y,dis_xi, dis_yi;
-    float distancia = 0, distancia_i = 0;
-    float dis_total = 0;
+    float repetidos = 0; //valor de penalizacion si el paso es repetido
+    float chocados = 0; //valor de penalizacion si se choca con obstáculos
+    int pasoActual;
+    int pasoSiguente;
+    //int j=0;
+    int k=0;
+    float posicion[][]; //este se supone que es el arreglo que viene de mi matriz fija
+    int pos_x=0, pos_y=0;
+    int ubi_x=0, ubi_y=0;
 
+    int pasosVacios = 327;
 
-    fitness = 1/(1 + dis_total);
-    printf ("\nFitness= %.3f\tDistancia Total= %.2f\n", fitness, dis_total);
+    //procedo a decodificar el cromosoma, para ver cuales cajas van dentro del contenedor
+    //char mask = 0x01;
+    for (int i=0; i < N_PASOS; i++)
+    {
+        if(pasosVacios==0){
+                pasoActual = *(cromosoma + i); //0,1,2,3
+                pasoSiguente = *(cromosoma + i + 1); //0,1,2,3
 
+                //j = pasoActual% N_MOV;
+                k = pasoSiguente% N_MOV;
+                
+                if(k==0){
+                    pos_y = pos_y + 1;
+                    if(posicion[pos_x][pos_y]==0){
+                        ubi_y = ubi_y + 1; 
+                    }else if(posicion[pos_x][pos_y]==1){
+                        ubi_y = ubi_y + 1;
+                        repetidos = repetidos + 1;
+                    }else{
+                         chocados =  chocados + 1;
+                         pos_y = pos_y - 1;
+                    }
+                }
+                
+                if(k==1){
+                    pos_x = pos_x + 1;
+                    if(posicion[pos_x][pos_y]==0){
+                        ubi_x = ubi_x + 1; 
+                    }else if(posicion[pos_x][pos_y]==1){
+                        ubi_x = ubi_x + 1;
+                        repetidos = repetidos + 1;
+                    }else{
+                         chocados =  chocados + 1;
+                         pos_x = pos_x - 1;
+                    }
+                }
+            
+                if(k==2){
+                    pos_y = pos_y - 1;
+                    if(posicion[pos_x][pos_y]==0){
+                        ubi_y = ubi_y - 1; 
+                    }else if(posicion[pos_x][pos_y]==1){
+                        ubi_y = ubi_y - 1;
+                        repetidos = repetidos + 1;
+                    }else{
+                         chocados =  chocados + 1;
+                         pos_y = pos_y + 1;
+                    }
+                }
+            
+                if(k==3){
+                    pos_x = pos_x - 1;
+                    if(posicion[pos_x][pos_y]==0){
+                        ubi_x = ubi_x - 1; 
+                    }else if(posicion[pos_x][pos_y]==1){
+                        ubi_x = ubi_x - 1;
+                        repetidos = repetidos + 1;
+                    }else{
+                         chocados =  chocados + 1;
+                         pos_x = pos_x + 1;
+                    }
+                }
+            //distancia = sqrt((pow(dis_x,2))+(pow(dis_y,2)));
+            //dis_total = dis_total + distancia + Penalizacion + distancia_i;
+            //Penalizacion = 0;
+            //distancia_i = 0;
+        }else{
+            i=N_PASOS;
+        }
+    }
+    //Ahora debo de calcular el error
+    //Para ello chequeo si excedo del mÃ¡ximo permitido. Si estoy dentro del rango, penalizo
 
-
-}
+    //ahora calculo el fitness, o valor neto, en funciÃ³n del ValorTotal, la Penalizacion y el VolumenRest
+    fitness = 1/(1 + repetidos + chocados);
+    printf ("\nFitness= %.3f\tDistancia Total= %.2f\n", fitness);
